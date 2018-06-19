0a6a84b527d8ed0f60930dad123b643f4c648b17
Zoey <1913327563@qq.com>
2018-06-19 10:50:37 +0800

    服务消费者

---
+++ src/main/java/inde/dubbo/Client.java
@@ -0,0 +1,29 @@
+package inde.dubbo;
+
+import inde.dubbo.api.IProvider;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+* @ClassName: Client
+* @Description: 客户端接收
+* @author 谢振海
+* @date 2017年9月26日 下午11:53:14
+*/
+public class Client {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "dubbo.xml" });
+		context.start();
+		IProvider demoService = (IProvider) context.getBean("providerService"); // 获取bean
+		String message = "";
+		try {
+			System.out.println("服务消费方");
+			message = demoService.build("客户端");
+			System.out.println("这个信息来自服务端:" + message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
